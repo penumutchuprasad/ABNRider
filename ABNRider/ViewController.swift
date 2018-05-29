745c8d8bdb15dd39f7ac35f853bdec6622cf4a05
penumutchu.prasad@gmail.com <penumutchu.prasadgmail.com@penumutchu-macbook-pro.local>
2018-05-30 00:07:21 +0530

    Initial Commit

---
+++ ABNRider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ABNRider
+//
+//  Created by penumutchu.prasad@gmail.com on 30/05/18.
+//  Copyright © 2018 abnboys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
